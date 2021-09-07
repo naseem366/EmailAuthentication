d1c02dfeef049b6f5c4a755459e8ba675cf5bb71
Md Khan <54537980+naseem366@users.noreply.github.com>
2021-09-07 18:07:34 +0530

    Update settings.py

--- EmailAuth/settings.py
+++ EmailAuth/settings.py
@@ -142,7 +142,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'akashiit366@gmail.com'
-EMAIL_HOST_PASSWORD = 'naseem123@'
+EMAIL_HOST_PASSWORD = '#'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default sending email'
 
